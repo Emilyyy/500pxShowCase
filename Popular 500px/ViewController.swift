50a04d0cea0993f72658fb5f9124db086277996a
Emilyyy <emilyliu@Emilys-MacBook-Pro.local>
2017-02-14 20:26:00 -0500

    Initial Commit

---
+++ Popular 500px/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Popular 500px
+//
+//  Created by Emily Liu on 2017-02-14.
+//  Copyright © 2017 Emily Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
